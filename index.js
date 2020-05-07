acdd2ee6c5cc556ac4d43e08b81e753d8000c219
royyuan <ryuan19@uw.edu>
2020-05-06 21:54:33 -0700

    done?

--- index.js
+++ index.js
@@ -4,7 +4,6 @@
  * Section AF with Jack Venburg
  * This is the index.js page. It fetches information about Pokemon abilities from the PokeAPI page.
  * It is used by my index.html page to allow the user to interact with buttons.
-
  */
 "use strict";
 
@@ -15,10 +14,16 @@
 
   window.addEventListener("load", init);
 
+  /**
+   * This function initializes the page and allows it to work.
+   */
   function init() {
     id("go-btn").addEventListener("click", goPokemon);
   }
 
+  /**
+   * Fetches the abilities of the Pokemon
+   */
   function goPokemon() {
     clear();
     pokemonName = id("input").value + "/";
@@ -28,9 +33,13 @@
       .then(convert => convert.json())
       .then(data => getData(data))
       .then(abilities => create(abilities))
-      .catch(err => error(err));
+      .catch(error);
   }
 
+  /**
+   * Resets the message being displayed, if it is either an error message or the Pokemon's
+   * abilities
+   */
   function clear() {
     let main = qs("main");
     while (main.contains(id("abilities"))) {
@@ -39,16 +48,27 @@
     }
   }
 
-  function error(err) {
-    make("Plese insert a valid Pokemon name")
+  /**
+   * Displays the error message if there is an error
+   */
+  function error() {
+    make("Plese insert a valid Pokemon name");
   }
 
+  /**
+   * Creates and displays the Pokemon's abilities
+   * @param {object} abilities - the JSON object's abilities
+   */
   function create(abilities) {
     for (let i = 0; i < abilities.length; i++) {
       make(abilities[i].ability.name);
     }
   }
 
+  /**
+   * Creates and appends a div tag with the text content set to the inputed element to main
+   * @param {string} element - the text you want as the text content
+   */
   function make(element) {
     let div = document.createElement("div");
     div.textContent = element;
@@ -56,10 +76,20 @@
     qs("main").appendChild(div);
   }
 
+  /**
+   * Returns the abilities of the Pokemon that the user inputed
+   * @param {object} data - the JSON object that the abilities are being retrieved from
+   * @returns {object} the JSON object's abilities
+   */
   function getData(data) {
     return data.abilities;
   }
 
+  /**
+   * Returns the response (URL) if it is valid, otherwise will throw an error
+   * @param {string} response - checks if the response (URL) exists/is valid
+   * @returns {string} the response (URL), returned only if it is valid
+   */
   function checkStatus(response) {
     if (response.ok) {
       return response;
@@ -68,10 +98,20 @@
     }
   }
 
+   /**
+   * Returns an element object based on the parameter element's (String) ID.
+   * @param {*} element - the element whose ID will be used
+   * @returns {object} the object that is associated with element's ID
+   */
   function id(idName) {
     return document.getElementById(idName);
   }
 
+  /**
+   * Returns an element object based on the parameter element's (String) class.
+   * @param {*} element - the element whose class will be used
+   * @returns {object} the object that is associated with element's ID
+   */
   function qs(selector) {
     return document.querySelector(selector);
   }
